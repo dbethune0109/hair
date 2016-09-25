b48827f218a359e9432c561ff7357a5c273485f7
David Bethune <dbethune1970@gmail.com>
2016-09-25 09:23:24 -0700

    changed password back to null

--- src/test/java/DatabaseRule.java
+++ src/test/java/DatabaseRule.java
@@ -1,10 +1,10 @@
-import org.junit.rules.ExternalResource;
+nullimport org.junit.rules.ExternalResource;
 import org.sql2o.*;
 
 public class DatabaseRule extends ExternalResource {
 
   protected void before() {
-    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/hair_salon_test", "postgres","fuku");
+    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/hair_salon_test", null,null);
    }
 
   protected void after() {
