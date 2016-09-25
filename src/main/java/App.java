fd502fe26d351f578d6d91b346d7b82a3a77f4e9
David Bethune <dbethune1970@gmail.com>
2016-09-25 11:44:01 -0700

    Update App.java
    added lines to app.java for heroku

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,6 +9,16 @@ public class App {
   public static void main(String[] args) {
     staticFileLocation("/public");
     String layout = "templates/layout.vtl";
+    
+       ProcessBuilder process = new ProcessBuilder();
+     Integer port;
+     if (process.environment().get("PORT") != null) {
+         port = Integer.parseInt(process.environment().get("PORT"));
+     } else {
+         port = 4567;
+     }
+
+    setPort(port);
 
 
   get("/", (request, response) -> {
